5195b63b30d260910184d88c9a49bd24fd3519ab
KevinK-ATU2023 <kkarundu8@gmail.com>
2024-08-26 17:01:43 +0100

    added middleware to server

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const port = 3000
 const express = require("express")
 const app = express()
 
-// database 
+// middleware
+const cors = require("cors")
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}); 
+
+// mongodb database 
 const mdb = require("mongoose")
 
 mongoose_connect().catch((err) => { console.log(err) });
@@ -12,6 +22,8 @@ mongoose_connect().catch((err) => { console.log(err) });
 async function mongoose_connect() {
     await mdb.connect('mongodb+srv://kkarundu8:aBKCgW168jnWz4WJ@cluster0.m2gfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 }
+
+
 
 app.get("/", ( req, res ) => {
     res.send("Hello from server!")
