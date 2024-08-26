332332b0d9d6e4decec2303b57cd3ad7dd00a07b
KevinK-ATU2023 <kkarundu8@gmail.com>
2024-08-26 17:24:07 +0100

    added body-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ const port = 3000
 const express = require("express")
 const app = express()
 
-// middleware
+const body_parser = require("body-parser")
+app.use(body_parser.urlencoded({ extended: false }))
+app.use(body_parser.json())
+
+// cors middleware
 const cors = require("cors")
 app.use(cors())
 app.use(function(req, res, next) {
