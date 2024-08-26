676cd89eaa49be940aee31239be0478139cd8168
KevinK-ATU2023 <kkarundu8@gmail.com>
2024-08-26 16:51:40 +0100

    mongo database connection made

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const port = 3000
 
+// basic server 
 const express = require("express")
 const app = express()
+
+// database 
+const mdb = require("mongoose")
+
+mongoose_connect().catch((err) => { console.log(err) });
+
+async function mongoose_connect() {
+    await mdb.connect('mongodb+srv://kkarundu8:aBKCgW168jnWz4WJ@cluster0.m2gfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+}
 
 app.get("/", ( req, res ) => {
     res.send("Hello from server!")
